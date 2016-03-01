6a62ddd5fa11a4d6fd856e6a8684bb006f348310
alanpropp <apropp@stanford.edu>
2016-02-29 19:57:02 -0800

    updated watson

--- homework/watson-preview/fetcher.py
+++ homework/watson-preview/fetcher.py
@@ -9,8 +9,8 @@ url3 = "https://farm2.staticflickr.com/1650/23610660263_fb3e97fd80_z_d.jpg"
 url4 = "https://farm2.staticflickr.com/1491/23869646269_e05eea7ed8_z_d.jpg"
 url5 = "https://upload.wikimedia.org/wikipedia/commons/8/8a/Kaiseradler_Aquila_heliaca_e_amk.jpg?download"
 
-URLS = {url1, url2, url3, url4, url5}
-URL_names = ["a.jpg", "b.jpg", "c.jpg", "d.jpg", "e.jpg"]
+URLS = [url1, url2, url3, url4, url5]
+URL_names = ["Hermann.jpg", "Soldier.jpg", "Jets.jpg", "Presidents.jpg", "Eagle.jpg"]
 count = 0
 
 for url in URLS:
